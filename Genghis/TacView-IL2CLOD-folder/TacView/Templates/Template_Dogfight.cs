4c41c997e3df272433621416ed87358c5e43b308
Brent Hugh <brent@brenthugh.com>
2026-08-29 16:27:46 -0500

    Added files needed in TacView-IL2CLOD-folder

---
+++ Genghis/TacView-IL2CLOD-folder/TacView/Templates/Template_Dogfight.cs
@@ -0,0 +1,11 @@
+﻿//$reference TacviewRecorder.dll
+using TacviewRecorder;
+
+public class Mission : TacviewMission
+{
+	public override void Inited()
+	{
+		MissionType = TypeOfMission.DogFight;
+		base.Inited();
+	}
+}
